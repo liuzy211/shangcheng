b779bd4c37a65d2d09e8d34427a9dff4a52e25a7
liuzy211 <liuzy211@163.com>
2018-11-08 23:37:16 +0800

    这是最后测试

--- shangcheng/src/main/java/bean/User.java
+++ shangcheng/src/main/java/bean/User.java
@@ -1,7 +1,13 @@
 package bean;
+
 /**
-这只是一个测试的文档说明，我在远程仓库已更改代码，测试是否可以更新下来
-*/
+ * 这只是一个测试的文档说明，我在远程仓库已更改代码，测试是否可以更新下来
+ */
+/**
+ * 下载成功，最后测试是否可以上传成功
+ * @author Administrator
+ *
+ */
 public class User {
 	private int id;
 	private String user_name;
@@ -9,39 +15,51 @@ public class User {
 	private String user_address;
 	private int user_age;
 	private String user_sex;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUser_name() {
 		return user_name;
 	}
+
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
 	}
+
 	public String getUser_passwd() {
 		return user_passwd;
 	}
+
 	public void setUser_passwd(String user_passwd) {
 		this.user_passwd = user_passwd;
 	}
+
 	public String getUser_address() {
 		return user_address;
 	}
+
 	public void setUser_address(String user_address) {
 		this.user_address = user_address;
 	}
+
 	public int getUser_age() {
 		return user_age;
 	}
+
 	public void setUser_age(int user_age) {
 		this.user_age = user_age;
 	}
+
 	public String getUser_sex() {
 		return user_sex;
 	}
+
 	public void setUser_sex(String user_sex) {
 		this.user_sex = user_sex;
 	}
