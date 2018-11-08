7c7554066886060c3aa6cc2d2fc54a64bb5f3c41
liuzy211 <liuzy211@163.com>
2018-11-08 23:03:33 +0800

    这是一个测试的项目

---
+++ shangcheng/src/main/java/bean/User.java
@@ -0,0 +1,47 @@
+package bean;
+
+public class User {
+	private int id;
+	private String user_name;
+	private String user_passwd;
+	private String user_address;
+	private int user_age;
+	private String user_sex;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_passwd() {
+		return user_passwd;
+	}
+	public void setUser_passwd(String user_passwd) {
+		this.user_passwd = user_passwd;
+	}
+	public String getUser_address() {
+		return user_address;
+	}
+	public void setUser_address(String user_address) {
+		this.user_address = user_address;
+	}
+	public int getUser_age() {
+		return user_age;
+	}
+	public void setUser_age(int user_age) {
+		this.user_age = user_age;
+	}
+	public String getUser_sex() {
+		return user_sex;
+	}
+	public void setUser_sex(String user_sex) {
+		this.user_sex = user_sex;
+	}
+
+}
