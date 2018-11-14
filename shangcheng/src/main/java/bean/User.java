fca368a43f8a171f70dda7d97286cd559b14db88
liuzy211 <liuzy211@163.com>
2018-11-14 17:53:16 +0800

    删除了

--- shangcheng/src/main/java/bean/User.java
+++ shangcheng/src/main/java/bean/User.java
@@ -1,14 +1,7 @@
 package bean;
 
 /**
- * 这只是一个测试的文档说明，我在远程仓库已更改代码，测试是否可以更新下来
- */
-/**
- * 下载成功，最后测试是否可以上传成功
- * 这是第二次测试，测试是否上传成功
- * 第二次测试已经收到，这是返回的
- * @author Administrator
- *
+ * 
  */
 public class User {
 	private int id;
